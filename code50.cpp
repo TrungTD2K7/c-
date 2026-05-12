db96781c60526a53c10b4c1e0740ab489a55b600
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-12 22:10:12 +0700

    first commit

---
+++ code50.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a , b;
+	cout << " nhap so nguyen a : " << endl;
+	cin >> a ;
+	cout << " nhap so nguyen b : " << endl;
+	cin >> b ; 
+	cout << "tong cua 2 so la : " << a + b << endl; 
+	return 0;
+}
+
