254b5bfca3eac69e5515f71a44ddd63921da0d2b
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-13 14:42:16 +0700

    tinh ngay thang nam

--- code53.cpp
+++ code53.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
-
-    return 0;
+    	int a ;
+	cin >> a;
+	cout << " Years " << a / 360 << endl;
+	cout << " Weeks " << a / 30 << endl;
+	cout << " Days "  << a << endl;
+	return 0;
 }
