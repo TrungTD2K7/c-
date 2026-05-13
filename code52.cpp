aefd2bea0163a803343136db7a35e1ff0e473e4e
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-13 14:15:37 +0700

    tinh toan c++

--- code52.cpp
+++ code52.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
+int main (){
+	int a = 125, b = 12345;
+    long ax = 1234567890;
+    short s = 4043;
+    float x = 2.13459;
+    double dx = 1.1415927;
+    char c = 'W';
+    unsigned long ux = 2541567890;
+    cout << a + c << endl;
+    cout << x + c << endl;
+    cout << dx + x << endl;
+    cout << ((int) dx) + ax << endl;
+    cout << a + x << endl;
+    cout << s + b << endl;
+    cout << ax + b << endl;
+    cout << s + c << endl;
+    cout << ax + c << endl;
+    cout << ax + ux <<endl;
     return 0;
 }
