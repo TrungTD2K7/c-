b76f9c48f6035bf37ab069adf212012b3ae0d616
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-14 19:27:34 +0700

    lenh temp

--- code57.cpp
+++ code57.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-
+        int a ;
+    int b ;
+    int temp ;
+    cout << " nhap so nguyen a : " <<endl;
+    cin >> a ;
+    cout << " nhap so nguyen b : " <<endl;
+    cin >> b ;
+    temp = a;
+    a = b ;
+    b = temp;
+    cout << a <<endl;
+    cout << b <<endl;
     return 0;
 }
