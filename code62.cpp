a56e4739ec5c479e72d52900f68e1c64f0414b2d
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-19 20:35:28 +0700

    lenh static_cast

--- code62.cpp
+++ code62.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-
+    int a ;
+    cin >> a;
+    int b ;
+    cin >> b;
+    cout << a / b <<endl;
+    cout << static_cast<double>(a) / b <<endl ;
     return 0;
 }
