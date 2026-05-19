d155f9e109b2c66f26dff73fb3f37a87be5aebb4
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-19 20:10:10 +0700

    luyen tap

--- code60.cpp
+++ code60.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-
+    int a ;
+    double b ;
+    cout << " nhap so nguyen a" <<endl;
+    cin >> a ;
+    cout << " nhap so thuc b " <<endl;
+    cin >> b ;
+    cout << " thuong : " << a / b <<endl;
+    cout << " tich : " << a * b <<endl;
+    cout << " tong : " << a + b <<endl;
+    cout << " hieu : " << a - b <<endl;
     return 0;
 }
