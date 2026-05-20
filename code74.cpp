91b8fd0f268c346bca30a493e41a5ed6257d291c
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-20 17:22:11 +0700

    switch case tim thang

--- code74.cpp
+++ code74.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
+int main(){
+    int a ;
+    cin >> a ;
+    switch(a){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+        cout << "Thang co 31 ngay";
+        break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+        cout << " thang co 30 ngay";
+        break;
+        case 2:
+        cout << " thang co 29 ngay ";
+        break;
+    }
     return 0;
 }
