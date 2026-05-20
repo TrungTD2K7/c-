e61a9faabe5403504df8415d9528b2beba052f3d
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-20 13:25:34 +0700

    elseif c++

--- code64.cpp
+++ code64.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
-
+    int d ;
+	cout << " nhap diem trung binh : " ;
+	cin >> d;
+	if( d >= 0 && d <= 5.0 ){
+		cout << " yeu ";
+	}
+	else if( d >= 5.0 && d <= 6.5){
+		cout <<" trung binh ";
+	}
+	else if( d >= 6.5 && d <= 8.0 ){
+		cout <<" kha ";
+	}
+	else if ( d >= 8.0 && d <= 10.0){
+		cout <<"gioi" ;
+	}
+	else{
+		cout << " svat ";
+	}
     return 0;
 }
