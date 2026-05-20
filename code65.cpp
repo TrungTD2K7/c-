7125feab33975a2015e780aabc82f306b0d7d361
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-20 13:50:02 +0700

    if long

--- code65.cpp
+++ code65.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
-
+    int a , b , c;
+	cin >> a ;
+	cin >> b ;
+	cin >> c ;
+	if( a == 0 ){
+		if( b == 0 ){
+			if ( c == 0 ){
+				cout << " vo so nghiem ";
+			}
+			else{
+				cout << " vo nghiem ";
+			}
+		}
+		else{
+			cout << " co 1 nghiem ";
+		}
+		
+	}
+	else{
+		cout << "tinh delta ";
+	}
     return 0;
 }
