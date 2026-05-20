6e3a611970707b4681e792c43cdf23604e3885f8
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-20 14:11:10 +0700

    add cpp files

--- code72.cpp
+++ code72.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-
+    	char a;
+	cout << "nhap ky tu thu : " ;
+	cin >> a ;
+	switch(a){
+		case '2' : cout << " thu hai "; break;
+		case '3' : cout << " thu ba "; break;
+		case '4' : cout << " thu tu "; break;
+		case '5' : cout << " thu nam "; break;
+		case '6' : cout << " thu hai "; break;
+		case '7' : cout << " thu hai "; break;
+		case '8' : cout << " thu hai "; break;
+	}
     return 0;
 }
