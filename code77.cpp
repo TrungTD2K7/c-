eb9900fc64e8fd7c898082de6c85cf02477b3648
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-21 21:04:56 +0700

    xep loai hsinh

--- code77.cpp
+++ code77.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-using namespace std;
 
-int main() {
+using namespace std ;
 
+int main(){
+    double a ;
+    cout << " nhap diem cua ban : ";
+    cin >> a ;
+    if( a >= 0 && a <=5 ){
+        cout << " ĐÚP !! ";
+    }
+    else if( a > 5 && a <= 6.5 ){
+        cout << " Trung binh ";
+    }
+    else if( a > 6.5 && a <= 8 ){
+        cout << " Kha ";
+    }
+    else if( a > 8 && a <= 9 ){
+        cout << " Gioi";
+    }
+    else if( a > 9 && a <= 10 ){
+        cout << " Xuat sac";
+    }
+    else{
+        cout << " chiu bo ";
+    }
     return 0;
 }
