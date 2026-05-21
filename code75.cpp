d4403803d08cdb9bf366f011c9ab440dddad9a52
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-21 20:31:44 +0700

    luyen tay

--- code75.cpp
+++ code75.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
-
+    int a , b ;
+    cout << " nhap so nguyen a : ";
+    cin >> a ;
+    if( a > 0 ){
+        cout << " la so nguyen duong "<<endl;
+    }
+    else{
+        cout << " la so nguyen am "<<endl;
+    }
     return 0;
 }
