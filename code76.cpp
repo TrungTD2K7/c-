d25e57f4ae5eb0d50a70e526f5ea23cb71e37704
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-21 20:39:05 +0700

    chan va le

--- code76.cpp
+++ code76.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
-
+    int a ;
+    cout << " nhap so nguyen a : ";
+    cin >> a ;
+    if( a % 2 == 0 ){
+        cout << " la so chan ";
+    }
+    else{
+        cout << " la so le ";
+    }
     return 0;
 }
