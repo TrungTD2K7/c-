485117ec89972d212db3d205b359ee12b6ae1190
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-22 13:20:30 +0700

    co ngiem

--- code79.cpp
+++ code79.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
+// Bài 1 — Phân loại tam giác
+// Nhập 3 cạnh a, b, c. Kiểm tra có tạo thành tam giác không,
+//  nếu có thì phân loại: đều / cân / vuông / thường.
+int main(){
+    double a , b ;
+    cout << " nhap so nguyen a "<<endl;
+    cin >> a ;
+    cout << " nhap so nguyen b "<<endl;
+    cin >> b ;
+    if ( a == 0 && b != 0){
+        cout << " vo nghiem " <<endl;
+    }
+    else if ( a == 0 && b == 0){
+        cout << " vo so nghiem " <<endl;
+    }
+    else{
+        cout << " co 1 nghiem la x = " << -b / a <<endl;
+    }
     return 0;
 }
