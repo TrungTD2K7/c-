34a6332c94e689c07d5cf0f7184702e7ad2da18d
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-23 19:28:34 +0700

    bang cuu chuong

--- code79.cpp
+++ code79.cpp
@@ -1,20 +1,13 @@
-// Bài 1 — Phân loại tam giác
-// Nhập 3 cạnh a, b, c. Kiểm tra có tạo thành tam giác không,
-//  nếu có thì phân loại: đều / cân / vuông / thường.
+#include <iostream>
+
+using namespace std;
+ 
 int main(){
-    double a , b ;
-    cout << " nhap so nguyen a "<<endl;
-    cin >> a ;
-    cout << " nhap so nguyen b "<<endl;
-    cin >> b ;
-    if ( a == 0 && b != 0){
-        cout << " vo nghiem " <<endl;
-    }
-    else if ( a == 0 && b == 0){
-        cout << " vo so nghiem " <<endl;
-    }
-    else{
-        cout << " co 1 nghiem la x = " << -b / a <<endl;
-    }
-    return 0;
+	int a , b ;
+	cout << " nhap so nguyen b ";
+	cin >> b ;
+	for ( a = 1 ; a <= 10 ; a++){
+		cout << a * b <<endl;
+	}
+	return 0 ;
 }
