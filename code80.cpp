5a1326cc88f8eb5ba9eeb28b2045e915fcb8718f
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-24 19:12:00 +0700

    tong so nguyen

--- code80.cpp
+++ code80.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
+int main(){
+    int i ;
+    int n ;
+    cout << " nhap so nguyen n : "<<endl;
+    cin >> n;
+    int sum = 0 ;
+    for( i = 0 ; i <= n ; i++ ){
+        sum += i;
+    }
+    cout << " tong cua n so hang la : " << sum;
     return 0;
 }
