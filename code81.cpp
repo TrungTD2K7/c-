b6142ea7e674859be05e539851a42db39938ce76
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-27 13:13:20 +0700

    vong lap for

--- code81.cpp
+++ code81.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
+    int i , s , n ;
+    cout << " nhap N : ";
+    cin >> n ;
 
+    // s = 1 + 2 + 3 + ... + n 
+    s = 0 ;
+    for ( i = 1 ; i <= n ; i++ ){
+        s = s + i ;
+    }
+    cout << " tong :  " << s ; 
     return 0;
 }
