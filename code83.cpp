dea3e9b26da1b931f525f3ce3e195340d3c7eb46
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-27 14:04:38 +0700

    for nang cao

--- code83.cpp
+++ code83.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-using namespace std;
 
-int main() {
+using namespace std ;
 
+int main (){
+    int i , n ;
+    float s = 0 ;
+    cout << " nhap n :  " ; 
+    cin >> n;
+    cout << " s = ";
+    for ( i = 1 ; i <= n ; i ++ ){
+        if ( i % 2 == 0 ){
+            s = s - 1.0 / i ;
+            cout << i << " - ";
+        }
+        else{
+            s = s + 1.0 / i;
+            cout << i << " + ";
+        }
+        cout << " tong :" << s ;
+    }
     return 0;
 }
