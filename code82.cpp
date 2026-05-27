378da0cc07b506a917f3738db4787c490d81c0bc
trungtran2909 <trungtran.tdtss@gmail.com>
2026-05-27 13:44:46 +0700

    vong lap for nang cao

--- code82.cpp
+++ code82.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-using namespace std;
+# include <iostream>
 
-int main() {
+using namespace std ;
 
+int main (){
+    int i , n , s ;
+    cout << " nhap n :  " ; 
+    cout << " s = ";
+    for ( i = 1 ; i <= n ; i++){
+        s = s + i;
+        if ( i == n ){
+            cout << i ;
+        }
+        else{
+            cout << i << " + ";
+        }
+    }
     return 0;
 }
