6eeaa57db8c40a939c945d9206bc38620369c524
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-02 18:14:31 +0700

    so lon thu hai

--- code91.cpp
+++ code91.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-using namespace std;
 
-int main() {
+using namespace std ; 
 
+int main(){
+    int h , c ;
+    cout << " nhap hang : ";
+    cin >> h ;
+    cout << " nhap cot : ";
+    cin >> c ; 
+    int A[h][c];
+    int i , j ;
+    int max1 = 0 ;
+    int max2 = 0;
+    for( i = 0 ; i < h ; i++ ){
+        for( j = 0 ; j < c ; j++ ){
+            cout << " nhap A [ " << i << " ] [ " << j << " ] ";
+            cin >> A[i][j];
+        }
+    }
+    for( i = 0 ; i < h ; i++ ){
+        for( j = 0 ; j < c ; j++ ){
+            if( max1 < A[i][j] ){
+                max2 = max1;
+                max1 = A[i][j];
+            }
+            else if( max1 > A[i][j] && max2 < A[i][j]){
+                max2 = A[i][j];
+            }
+        }
+    }
+    cout << max2;
     return 0;
 }
