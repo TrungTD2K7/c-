813efafa4d4b828ef8ffc4a1fe99b3600a9c7003
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-02 18:38:13 +0700

    tim so be thu hai

--- code92.cpp
+++ code92.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
+int main(){
+    int r , d ;
+    cout << " nhap hang : ";
+    cin >> r ;
+    cout << " nhap cot : ";
+    cin >> d ;
+    int A[r][d];
+    int i , j ;
+    int min1 = 1000;
+    int min2 = 900 ;
+    for(i = 0 ; i < r ; i++){
+        for(j = 0 ; j < d ; j++){
+            cout << " nhap A [ " << i << " ] [ " << j << " ] ";
+            cin>>A[i][j];
+        }
+    }
+    for( i = 0 ; i < r ; i++){
+        for ( j = 0 ; j < d ; j++){
+            if( A[i][j] < min1 ){
+            	min2 = min1;
+            	min1 = A[i][j];
+			}
+			else if( min1 > A[i][j] && min2 < A[i][j] ){
+				min2 = A[i][j];
+			}
+        }
+    }
+    cout << min2;
     return 0;
 }
