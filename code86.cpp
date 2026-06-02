f0f0d2b01c0583e0af1fe76683b6932de25cf925
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-02 12:53:32 +0700

    ve hinh chu nhat

--- code86.cpp
+++ code86.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <iostream> 
+
 using namespace std;
 
-int main() {
-
-    return 0;
+int main(){
+    int dai , rong , i , j ;
+    cout << " nhap chieu dai : ";
+    cin >> dai ;
+    cout << " nhap chieu rong : ";
+    cin >> rong ;
+    for( i = 0 ; i < rong ; i++){
+        for(j = 0 ; j < dai ; j++){
+            cout << "*";
+        }
+        cout <<endl;
+    }
 }
