4c7f7dc6ba2296d810caec5f957c240adf91ea42
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-02 13:31:38 +0700

    ve hinh chu nhat rong

--- code87.cpp
+++ code87.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
-    return 0;
+int main(){
+    int dai , rong , i , j ;
+    cout << " nhap chieu rong ";
+    cin >> rong ;
+    cout << " nhap chieu dai ";
+    cin >> dai ;
+    for( i = 0 ; i < rong ; i++ ){
+        for( j = 0 ; j < dai ; j++ ){
+            if( i == 0 || i == rong -1 || j == 0 || j == dai -1 ){
+                cout << " *";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
 }
