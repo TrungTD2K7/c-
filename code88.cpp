b88df82d86e323105045600b42dc6473e0be0ce1
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-02 16:16:17 +0700

    ma tran

--- code88.cpp
+++ code88.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
+int main(){
+    int r , d ;
+    cout << " nhap hang : ";
+    cin >> r ;
+    cout << " nhap cot : ";
+    cin >> d ;
+    int A[r][d];
+    int i , j ;
+    for(i = 0 ; i < r ; i++){
+        for(j = 0 ; j < d ; j++){
+            cout << " nhap A [ " << i << " ] [ " << j << " ] ";
+            cin>>A[i][j];
+        }
+    }
+    for( i = 0 ; i < r ; i++){
+        for ( j = 0 ; j < d ; j++){
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
