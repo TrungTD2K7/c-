6b890d54bfe06ad0f9ffb1bb1be066eb11692dbb
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-09 22:14:56 +0700

    luyen pt 1

--- code95.cpp
+++ code95.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main() {
-
+    int i , n ; 
+    cout << " so phan tu nhap vao la : ";
+    cin >> n;
+    int A[i];
+    int dem = 0 ;
+    int sum = 0 ;
+    for( i = 0 ; i < n ; i ++  ){
+        cout << " nhap A [ " << i << " ] " ;
+        cin >> A[i] ;
+    }
+    for ( i = 0 ; i < n ; i ++){
+        if ( i % 2 == 0 ){
+            sum = sum + A[i];
+        }
+    }
+    cout << " tong cua cac so chan la : " << sum <<endl;
+    for( i = 0 ; i < n ; i ++ ){
+        if ( i % 3 == 0 ){
+            cout << A[i] << " la so chia het cho 3 "<<endl;
+            dem++;
+        }
+    }
+    cout << " co " << dem << " so chia het cho 3 ";
     return 0;
 }
