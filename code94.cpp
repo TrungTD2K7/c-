c12d7e11922a74219cf9a8efe68edb6acd9ff858
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-10 22:14:07 +0700

    luyen tay co ban

--- code94.cpp
+++ code94.cpp
@@ -48,10 +48,32 @@ int main(){
                                 tien = 15000 + (i - 1) * 13500;
                             }
                             else if( i >= 31 ){
-                                tien = 15000 + 30 * 13.500 + ( i - 30) * 11000;
+                                tien = 15000 + 29 * 13500 + ( i - 30) * 11000;
                             }
                             cout << " chi phi phai tra la : "<< tien <<endl;
                             break;
+                        }
+                        case 3 :{
+                            int ngay , thang , nam ;
+                            cout << " nhap nam cua ban :";
+                            cin >> nam;
+                            cout << " nhap thang cua ban : ";
+                            cin >> thang ;
+                            switch[thang]{
+                                case 1 : case 3 : case 5 : case 7 : case 8 : case 10 : case 12 :
+                                ngay = 31;
+                                case 4 : case 6 : case 8 : case 9 : case 11 : 
+                                ngay = 30;
+                                case 2 :{
+                                    if( nam % 4 == 0 && nam % 100 == 0 ){
+                                        ngay = 29 ;
+                                    }
+                                    else{
+                                        ngay = 28 ;
+                                    }
+                                }
+                            }
+                            cout << " nam " << nam << " thang " << thang << " co " << ngay << " ngay ";
                         }
                     }
                 }while( choice2 !=4 );
