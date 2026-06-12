6aa2299fc481e009bb0ee3f290a8bb7cde085c80
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-12 18:31:12 +0700

    vong lap va ham

--- code96.cpp
+++ code96.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
+int main(){
+    int h , c;
+    cout << " nhap so hang :  ";
+    cin >> h ;
+    cout << " nhap so cot : ";
+    cin >> c ;
+    int A[h][c];
+    int i , j ; 
+    for( i = 0 ; i < h ; i++ ){
+        for( j = 0 ; j < c ; j++ ){
+            cout << " Nhap A [ " << i << " ] [ " << j << " ] ";
+            cin >> A[i][j];
+        }
+    }
+    for( i = 0 ; i < h ; i++ ){
+        for ( j = 0 ; j < c ; j++ ){
+            if(  A[i][j] % 2 == 0 ){
+                cout << A[i][j] << " " ;
+            }
+            else {
+                cout << 0 << " " ;
+            }
+        }
+        cout <<endl;
+    }
     return 0;
 }
