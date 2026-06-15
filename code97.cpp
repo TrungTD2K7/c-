c325bec76901abbf198ded168cfefb6024331e6c
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-15 16:10:05 +0700

    con tro

--- code97.cpp
+++ code97.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void swap( int *a , int *b ){
+    int t = *a;
+    *a = *b ;
+    *b = t ;
+}
 
+int main() {
+    int a , b ; 
+    cout << " nhap a : " ; 
+    cin >> a ; 
+    cout << " nhap n : ";
+    cin >> b ;
+    swap(a,b);
+    cout << a <<endl;
+    cout << b <<endl;
     return 0;
 }
