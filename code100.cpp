f7973bb9ec5256cbfdb8fd0591efd1bb86e8980e
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-18 14:44:48 +0700

    so nguyen to

--- code100.cpp
+++ code100.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
+void snt( int n){
+    bool kq = true;
+    for ( int i = 2 ; i < n / 2 ; i++){
+        if ( n % i == 0 ){
+            kq = false;
+        }
+    }
+    if(kq){
+        cout << " la so nguyen to  ";
+    }
+    else{
+        cout << " khong phai so nguyen to ";
+    }
+}
+int main (){
+    int m ;
+    cout << " nhap so nguyen m : ";
+    cin >> m ; 
+    snt(m);
     return 0;
 }
