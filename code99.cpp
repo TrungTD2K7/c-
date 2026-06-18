6f0139a018e4cc4ec7dcbadc16c91d76e7f67202
trungtran2909 <trungtran.tdtss@gmail.com>
2026-06-18 09:03:50 +0700

    mang so dao

--- code99.cpp
+++ code99.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-    return 0;
+int dn( int n){
+	int d = 0;
+	while ( n != 0){
+	    d = n % 10 + d * 10 ;
+		n = n / 10;
+	}
+	return d ;
+}
+int main (){
+	int m ;
+	cout << " nhap so nguyen : ";
+	cin >> m ;
+	cout << " so dao nguoc la : ";
+	cout << dn(m);
+	return 0 ;
 }
